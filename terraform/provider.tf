37558d0692ed0c0fc9e88bb9112f21895eafb21a
lifeisgenie <walker3z@naver.com>
2025-11-25 15:31:40 +0000

    feat: Add provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    cloudstack = {
+      source  = "cloudstack/cloudstack"
+      version = "~> 0.5.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+variable "api_key" {
+  description = "CloudStack API Key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "CloudStack Secret Key"
+  type        = string
+}
+
+provider "cloudstack" {
+  api_url    = "https://dku.kloud.zone/client/api"
+  api_key    = var.api_key
+  secret_key = var.secret_key
+}
