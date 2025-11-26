ae231a2dfd25c416d7423e94b6891380575c326e
lifeisgenie <walker3z@naver.com>
2025-11-26 09:09:44 +0000

    chore: automate Inventory file generation from Terraform outputs

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "cloudstack/cloudstack"
       version = "~> 0.5.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
   required_version = ">= 1.0"
 }
